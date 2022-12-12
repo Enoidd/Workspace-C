cf8fd16a020dedd60ac632c65af3512a97dbfabf
Enoidd <enoid994@gmail.com>
2022-12-12 17:44:34 +0100

    aggiornamento 4 Homework

--- 000_Homeworks/main.c
+++ 000_Homeworks/main.c
@@ -18,13 +18,17 @@ int dueComuni(int A[], int lA, int B[], int lB){
 			}
 		}
 	}
+	/* Hai contato due elementi uguali? */
 	if(count==2)
-		count = 1;
+		count = 1;	// Si
 	else
-		count = 0;
+		count = 0;	// No
 	
-	return count;
+	return count;	// Ritorna il valore degli elementi contati
 }
+
+/* ====================== QUINTO HOMERWORK ====================== */
+
 
 /*
  * Scrivi la funzione in linguaggio C int originale(int* A, int lenA, int* B, int lenB) che
@@ -33,11 +37,38 @@ int dueComuni(int A[], int lA, int B[], int lB){
  * ritorna 0 (false) se tutti gli elementi di A sono in B. Se A ha lunghezza zero la funzione ritorna 0 (false)
 */
 int originale(int* A, int lenA, int* B, int lenB) {
-	/* HP. Verifica Universale */
-	
+	int verificato = 1;	// inizialmente tutti gli elementi di A sono in B
+
+	/* Se la lunghezza di A è uguale a 0 e B ha lung minore di A */
+	if(lenA==0 || (lenA>lenB))
+		verificato = 0;
+	else{ // Altrimenti A ha lung >0
+		/* Se la lunghezza di B è diversa da 0 */
+		if(lenB!=0){
+			/* confronta i due array e verifica se gli elementi correnti sono uguali */
+			for(int i=0; i<lenA; i++){
+				for(int j=0; j<lenB; j++){
+					/* Se l'elemento corrente di A è uguale all'elemento corrente di B */
+					if(A[i]!=B[j])
+						verificato = 0;
+				}
+			}
+		}
+	}
+	return verificato;
 }
 
 /* ============================================================== */
+
+/* ====================== SESTO HOMERWORK ====================== */
+
+
+
+
+
+/* ============================================================== */
+
+
 int main(int argc, char **argv)
 {
 	int lA = 5;
