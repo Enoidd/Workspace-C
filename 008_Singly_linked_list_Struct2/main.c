2b1d78dcfb230f8faa0ac7788a618737e42f2342
Enoidd <113771201+Enoidd@users.noreply.github.com>
2022-12-18 21:45:09 +0100

    ultima modifica

--- 008_Singly_linked_list_Struct2/main.c
+++ 008_Singly_linked_list_Struct2/main.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 
 /* Struttura nodo della lista */
-typedef struct l{
+typedef struct nodo{
     int info;
-    struct l* next;
-}elist;
+    struct nodo* next;
+}nodo; // elist è il nodo della list 
 
-typedef elist* plist;   // lista
+typedef nodo* lista;   // plist è un puntatore a elist
 
 /* Funzione che crea una lista */
-plist new_lista(){
-    plist new = malloc(sizeof(plist));  // alloca memoria per una nuova lista
+lista new_lista(){
+    lista  new = malloc(sizeof(lista));  // alloca memoria per una nuova lista
     new = NULL; // lista vuota
 
     return new;
 }
 
 /* Funzione che stampa una lista */
-void print_lista(plist l){
-    if(l==NULL){
-        printf("\nError underflow. Lista vuota. Print fallita.\n");
+void print_lista(lista  l){
+    if(*(&l)==NULL){
+        printf("\nError underflow. Lista vuota. Print fallita.\n\n");
         return;
     }
     // Lista non vuota
@@ -31,10 +31,27 @@ void print_lista(plist l){
     printf("NULL\n");
 }
 
+/* Funzione che inserisce un elemento in testa alla lista */
+void insert(lista* l, int v){
+    nodo* new = malloc(sizeof(lista ));
+    new->info = v;
+    new->next = *l;
+    *l = new;
+}
+
 int main(){
 
     /* Lista l1 */
-    plist l1 = new_lista();
+    nodo* l1 = new_lista();
 
     print_lista(l1);    // Atteso: Error underflow. Lista vuota. Print fallita.
+
+    /* Inserimenti in testa a 'l1' */
+    insert(&l1, 1);
+    insert(&l1, 2);
+    insert(&l1, 3);
+
+    print_lista(l1);    // [3]---[2]---[1]---NULL
+    
+   
 }
