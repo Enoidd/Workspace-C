873b28d136dd33497717b1908910efa4f8de5ed4
Enoidd <113771201+Enoidd@users.noreply.github.com>
2022-12-18 17:44:14 +0100

    Modifica singly list

--- 008_Singly_linked_list_Struct2/main.c
+++ 008_Singly_linked_list_Struct2/main.c
@@ -9,8 +9,32 @@ typedef struct l{
 
 typedef elist* plist;   // lista
 
+/* Funzione che crea una lista */
+plist new_lista(){
+    plist new = malloc(sizeof(plist));  // alloca memoria per una nuova lista
+    new = NULL; // lista vuota
 
+    return new;
+}
+
+/* Funzione che stampa una lista */
+void print_lista(plist l){
+    if(l==NULL){
+        printf("\nError underflow. Lista vuota. Print fallita.\n");
+        return;
+    }
+    // Lista non vuota
+    while(l!=NULL){
+        printf("[%d]--", l->info);
+        l = l->next;    // passa al prossimo nodo della lista
+    }
+    printf("NULL\n");
+}
 
 int main(){
-    printf("Hello World");
+
+    /* Lista l1 */
+    plist l1 = new_lista();
+
+    print_lista(l1);    // Atteso: Error underflow. Lista vuota. Print fallita.
 }
